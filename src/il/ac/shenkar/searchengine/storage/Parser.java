e447f6fee29224fcf6b14d437407e0e563dafd04
Gregory <kogrego@gmail.com>
2016-10-12 14:17:39 +0300

    started parser implementation

--- src/il/ac/shenkar/searchengine/storage/Parser.java
+++ src/il/ac/shenkar/searchengine/storage/Parser.java
@@ -2,10 +2,18 @@ package il.ac.shenkar.searchengine.storage;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Parser {
 
     public ArrayList<String> parse(File toParse){
+        Scanner sc = new Scanner(toParse.getName());
+        ArrayList<String> words = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            Collections.addAll(words, line.split(" "));
+        }
         return null;
     }
 }
