d31a14cd658c33f76e7041afbd76680d700dee15
DassiShenkar <dassi.shenkar@gmail.com>
2016-10-13 14:01:47 +0300

    basic gui and changes to PostingData

--- src/il/ac/shenkar/searchengine/gui/Form.java
+++ src/il/ac/shenkar/searchengine/gui/Form.java
@@ -1,16 +1,52 @@
 package il.ac.shenkar.searchengine.gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class Form extends  JFrame {
-
+/**
+ * Created by Seymore on 10/13/2016.
+ */
+public class Form extends JFrame {
     private JButton loadFileSButton;
-    private JPanel panel;
+    private JPanel rootPanel;
+    private JTextField searchField;
+    private JButton searchButton;
+    private JLabel searchLabel;
 
     public Form() {
-        super("Search Engine");
+        super("search engine");
+
+        setContentPane(rootPanel);
+
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        loadFileSButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // process file
+            }
+        });
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                searchLabel.setText("results for: " + searchField.getText());
+                // search
+            }
+        });
+
+        searchField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    searchLabel.setText("results for: " + searchField.getText());
+                }
+            }
+        });
 
         setVisible(true);
     }
